db88676ff854b47f210c24099584d7da984ee88c
U0194109 <pmoure@viewnext.com>
2021-05-22 17:23:37 +0200

    first commit

---
+++ src/main/java/com/pablom/imatiaorders/ImatiaOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.pablom.imatiaorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImatiaOrdersApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ImatiaOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablom/imatiaorders/ImatiaOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablom.imatiaorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImatiaOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
